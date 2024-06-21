cb4e7b894d9a500b1e471fdac8434df73cb97fc2
Jorge Miguel Mendoza Correa <jorge.mendoza@bodesa.mx>
2024-06-21 13:17:57 -0600

    add ToSHA256

--- Extencions/String/StringExtensions.cs
+++ Extencions/String/StringExtensions.cs
@@ -1,6 +1,14 @@
-﻿namespace Red.Crypto.Extensions.String
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Red.Crypto.Extencions.String
 {
-    internal class StringExtensions
+    public static class StringExtensions
     {
+        public static string ToSHA256(this string str)
+        {
+            byte[] hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(str));
+            return Convert.ToHexString(hashBytes);
+        }
     }
 }
